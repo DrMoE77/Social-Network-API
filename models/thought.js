28384c0e1b54403b4075e948443d95148c8adce3
DrMoE77 <drmo.afifi@gmail.com>
2022-04-06 22:24:02 +1000

    server

---
+++ models/thought.js
@@ -0,0 +1,20 @@
+// Thought text 
+    //string
+    // require
+    // between 1-200 characters
+
+    // 
+    `createdAt`
+    
+    // Date
+     
+   // Set default value to the current timestamp
+    // Use a getter method to format the timestamp on query
+  
+  //`username` (The user that created this thought)
+  // String
+  // Required
+  
+  //`reactions` (These are like replies)
+    // Array of nested documents created with the `reactionSchema`
+  
