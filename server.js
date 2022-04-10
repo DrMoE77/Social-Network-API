786d2a45af194d278c1060fefea063c7247b594c
DrMoE77 <drmo.afifi@gmail.com>
2022-04-10 23:03:31 +1000

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const Router = require("./routes")
+
 
 const app = express()
 app.use(express.json())
@@ -16,7 +16,7 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
-app.use(Router);
+
 
 app.listen(3000, () => {
   console.log("Server is running at port 3000");
