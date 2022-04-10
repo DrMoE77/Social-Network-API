87a5a92ef2b0f3662018d017d5a27017e77bf57d
DrMoE77 <drmo.afifi@gmail.com>
2022-04-10 22:12:49 +1000

    thought

---
+++ routes/thought_route.js
@@ -0,0 +1,141 @@
+const router = require('express').Router();
+const {user,thought} = require("../models")
+
+// routing the GET request
+router.get('/', (req, res) => {
+  // Get all thoughts
+  thought.find({}).populate({path:"reactions"})
+    .then(dbThoughts => res.json(dbThoughts))
+    // incase of error
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// getting one thought by its id
+// routing the GET request by id --> also check if the id exists
+router.get('/:id', (req, res) => {
+  // find a single thought by its `id`
+  
+  thought.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).populate({path:"reactions"})
+    .then(dbUsers => {
+      // if tag doesn't exist
+      if (!dbThoughts) {
+        res.status(404).json({ message: 'Sorry, a thought with this id was not found!'});
+        return;
+      }
+      res.json(dbThoughts);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// adding a new thought
+// routing the POST request
+router.post('/', (req, res) => {
+  // create a new thought
+  thought.create({
+    thoughtText:req.body.thoughtText,username: req.body.username })
+    // To Do - adding this thought to a user id
+    .then(dbThoughts => res.json(dbThoughts))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
+});
+
+//updating thought info
+// routing the UPDATE request by id --> also check if the id exists
+router.put('/:id', (req, res) => {
+  // update a thought  by its `id` value
+  thought.findOneAndUpdate(req.body, {
+    where: {
+        id: req.params.id
+    }
+  })
+    .then(dbThoughts => {
+      // if thought doesn't exist
+        if (!dbThoughts[0]) {
+            res.status(404).json({ message: 'Sorry, thought with this id was not found!'});
+            return;
+        }
+        res.json(dbUsers);
+  })
+    .catch(err => {
+        console.log(err); 
+        res.status(500).json(err);
+  });
+
+});
+
+// deleting a user
+// routing the DELETE request by id --> also check if the id exists
+router.delete('/:id', (req, res) => {
+  // delete on user by its `id` value
+  user.findOneAndDelete({
+    where: {
+        id: req.params.id
+    }
+  })
+    .then(dbUsers => {
+      // if user doesn't exist
+        if (!dbUsers) {
+            res.status(404).json({ message: 'Sorry, a user with this id was not found!'});
+            return;
+        }
+        res.json(dbUsers);
+  })
+  // incase of error
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
+});
+
+// adding a friend to a user's list
+router.post('/:id', (req, res) => {
+  user.findOneAndUpdate
+    (req.body, {
+      where: {
+          id: req.params.id
+      },
+   $push:{friends:params.friendId}
+  })
+    .then(dbUsers => res.json(dbUsers))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
+});
+
+// deleting a friend from a user's list
+router.delete('/:id', (req, res) => {
+  user.findOneAndUpdate
+    (req.body, {
+      where: {
+          id: req.params.id
+      },
+   $pull:{friends:params.friendId}
+  })
+  .populate({path:"friends"})
+  .then(dbUsers => {
+    // if tag doesn't exist
+    if (!dbUsers) {
+      res.status(404).json({ message: 'Sorry, a user with this id was not found!'});
+      return;
+    }
+    res.json(dbUsers);
+  })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
+});
+module.exports = router;
