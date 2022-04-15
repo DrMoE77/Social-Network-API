d14773b6b6e560bc7fd7d7d3f318e7be909b8f22
DrMoE77 <drmo.afifi@gmail.com>
2022-04-15 23:07:23 +1000

    videos

--- server.js
+++ server.js
@@ -1,33 +1,29 @@
-// EXPRESS 
+//installing dependencies
 const express = require('express')
 const app = express()
 const PORT = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// MONGOOSE 
+// connecting to mongodb
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://127.0.0.1:27017/Social-Network-Api', {
+const uri = "mongodb://127.0.0.1:27017/Social-Network-Api"
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-
 mongoose.connection.on('connected', () =>
-console.log('Connected to MongoDB Endpoint')
+console.log('Connection to MongoDB successful!')
 );
-
+//incase of error in connection
 mongoose.connection.on('error', (err) =>
-console.log(`MONGOOSE DISCONNECTED ERROR: ${err}`)
+console.log(`Error-Disconnection: ${err}`)
 );
-
 mongoose.set('debug', true)
 
-// FILES
 app.use(require('./routes'));
 
-
-// Server Listen 
+//Starting the server 
 app.listen(PORT, () => {
 console.log((`Connected on localhost:${PORT}`))
 })
